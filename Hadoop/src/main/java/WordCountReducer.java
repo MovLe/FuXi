3c4520aa188e0713feb4a25d35fae26ed0d94dcb
Movle <richard.xiu.520@gmail.com>
2020-10-06 23:25:25 +0800

    Hadoop之wordcount程序的reducer

--- Hadoop/src/main/java/WordCountReducer.java
+++ Hadoop/src/main/java/WordCountReducer.java
@@ -18,5 +18,11 @@ public class WordCountReducer extends Reducer<Text, IntWritable,Text,IntWritable
     @Override
     protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
 
+        int count = 0;
+        for(IntWritable value:values){
+            count+=value.get();
+        }
+
+        context.write(key,new IntWritable(count));
     }
 }
