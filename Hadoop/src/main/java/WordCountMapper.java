c67aab86b7802a6e45fa48c733c4add539911227
Movle <richard.xiu.520@gmail.com>
2020-10-06 10:58:22 +0800

    新建大数据复习项目

---
+++ Hadoop/src/main/java/WordCountMapper.java
@@ -0,0 +1,13 @@
+/**
+ * @ClassName WordCountMapper
+ * @MethodDesc: TODO WordCountMapper功能介绍
+ * @Author Movle
+ * @Date 10/6/20 10:56 上午
+ * @Version 1.0
+ * @Email movle_xjk@foxmail.com
+ **/
+
+
+public class WordCountMapper {
+
+}
