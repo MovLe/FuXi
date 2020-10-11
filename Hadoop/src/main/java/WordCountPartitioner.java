889c9cd7f43280c3e222f19595de5af03de02f94
Movle <richard.xiu.520@gmail.com>
2020-10-11 15:41:01 +0800

    Hadoop之wordcount程序成功

--- Hadoop/src/main/java/WordCountPartitioner.java
+++ Hadoop/src/main/java/WordCountPartitioner.java
@@ -15,13 +15,13 @@ import org.apache.hadoop.mapreduce.Partitioner;
 public class WordCountPartitioner extends Partitioner<Text, IntWritable> {
     @Override
     public int getPartition(Text key, IntWritable value, int numPartitions) {
-        String firword = key.toString().substring(0,1);
+        String firWord = key.toString().substring(0,1);
 
-        char[] charArray = firword.toCharArray();
+        char[] charArray = firWord.toCharArray();
 
         int result = charArray[0];
 
-        if(result % 2 ==0 ){
+        if(result % 2 == 0 ){
             return 0;
         }else {
             return 1;
